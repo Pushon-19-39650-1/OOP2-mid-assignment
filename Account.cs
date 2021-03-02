e140baa4e89d32160a34d814079d43c592f44649
Pushon <reshme.pushon2012@gmail.com>
2021-03-02 13:11:50 +0600

    Mid Assignment First Version

---
+++ Account.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace MidTermLabAssignment1
+{
+    class Account
+    {
+        private int accountNumber;
+        private string accountName;
+        private double balance;
+        private Address address;
+
+        Bank objBnak = new Bank();
+
+        public Account()
+        {
+
+        }
+        public Account(string accountName, double balance, int acn, Address address)
+        {
+            this.accountName = accountName;
+            this.balance = balance;
+            this.address = address;
+            this.accountNumber = acn;
+        }
+
+
+
+        public int AccountNumber
+        {
+            get { return this.accountNumber; }
+            set { this.accountNumber = value; }
+        }
+        public string AccountName
+        {
+            get { return this.accountName; }
+            set { this.accountName = value; }
+        }
+        public double Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+        public Address Address
+        {
+            get { return this.address; }
+            set { this.address = value; }
+        }
+
+        public void Withdraw(double amount)
+        {
+            Console.WriteLine("Enter account number: ");
+            int accNo = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < objBnak.MyBank.Length; i++)
+            {
+                if (objBnak.MyBank[i] == null)
+                {
+                    continue;
+                }
+                else if (objBnak.MyBank[i].accountNumber == accNo)
+                {
+                    Console.WriteLine("Account found!!");
+                    objBnak.MyBank[i].Balance = objBnak.MyBank[i].Balance - amount;
+                    Console.WriteLine("Your Current Balance is: " + objBnak.MyBank[i].Balance);
+                    // flag = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Account not found!!");
+                }
+                // return balance;
+            }
+
+        }
+
+        //public void Transfer(Account receiver,double amount)
+        public void Transfer(double amount)
+        {
+            Console.WriteLine("Enter account number of receiver: ");
+            int accNo1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter account number of sender: ");
+            int accNo = Convert.ToInt32(Console.ReadLine());
+           
+            for (int i = 0; i < objBnak.MyBank.Length; i++)
+            {
+                if (objBnak.MyBank[i] == null)
+                {
+                    continue;
+                }
+                else if (objBnak.MyBank[i].accountNumber == accNo)
+                {
+                    Console.WriteLine("Account found!!");
+                    objBnak.MyBank[i].Balance = objBnak.MyBank[i].Balance - amount;
+
+                    Console.WriteLine("Your Current Balance is: " + objBnak.MyBank[i].Balance);
+                    
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Account not found!!");
+                }
+            }
+
+            for (int i = 0; i < objBnak.MyBank.Length; i++)
+            {
+                
+                if (objBnak.MyBank[i].accountNumber == accNo1)
+                {
+                    objBnak.MyBank[i].Balance = objBnak.MyBank[i].Balance + amount;
+                    Console.WriteLine("Transfer Compelted!!");
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+        }
+
+        public void Deposit(double amount)
+        {
+
+            Console.WriteLine("Enter account number: ");
+            int accNo = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < objBnak.MyBank.Length; i++)
+            {
+                if (objBnak.MyBank[i] == null)
+                {
+                    continue;
+                }
+                else if (objBnak.MyBank[i].accountNumber == accNo)
+                {
+                    Console.WriteLine("Account found!!");
+                    objBnak.MyBank[i].Balance = objBnak.MyBank[i].Balance + amount;
+                    Console.WriteLine("Your Current Balance is: " + objBnak.MyBank[i].Balance);
+                   
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Account not found!!");
+                }
+               
+            }
+        }
+
+        public void PrintAccount()
+        {
+
+            Console.WriteLine("\nAccount No:{0}\nAccount Name:{1}\nBalance:{2}", this.accountNumber, this.accountName, this.balance);
+            this.address.PrintAddress();
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTermLabAssignment1
+{
+    class Program
+    {
+
+        static int AccountNumberGen(int inc)
+        {
+            int start = 999;
+            int an = start + inc;
+            return an;
+        }
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            int num;
+
+            Account acc = new Account();
+            Console.WriteLine("How many accounts do you want to create? ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Bank ourBank = new Bank("Developer's bank", num);
+
+            while (flag)
+            {
+                Console.WriteLine("1) Create Account \n2) Deposit \n3) Withdraw \n4) Transfer \n5) Delete \n6) Print \n7) Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+
+                    case 1:
+
+
+                        for (int i = 0; i < num; i++)
+                        {
+                            Console.WriteLine("\nAccount " + (i + 1));
+                            Console.WriteLine("Name: ");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Ammount: ");
+                            double ammount = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("House no: ");
+                            int houseNo = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Road no: ");
+                            int roadNo = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("City: ");
+                            string city = Console.ReadLine();
+                            Console.WriteLine("Country: ");
+                            string country = Console.ReadLine();
+
+                            ourBank.AddAccount(new Account(name, ammount, AccountNumberGen(i), new Address(houseNo, roadNo, city, country)));
+
+                        }
+                        break;
+                    case 2:
+                        //double ammount = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("\nDeposit ammount: ");
+                        double dep = Convert.ToInt32(Console.ReadLine());
+                        acc.Deposit(dep);
+                        break;
+                    case 3:
+                        Console.WriteLine("\nWithdraw ammount: ");
+                        double withd = Convert.ToInt32(Console.ReadLine());
+                        acc.Withdraw(withd);
+                        break;
+                    case 4:
+                        Console.WriteLine("\nTransfer ammount: ");
+                        double trans = Convert.ToInt32(Console.ReadLine());
+                        
+                        acc.Transfer(trans);
+                        break;
+                    case 5:
+
+                        Console.WriteLine("House no: ");
+                        int delAC = Convert.ToInt32(Console.ReadLine());
+                        ourBank.DeleteAccount(delAC);
+                        break;
+                    case 6:
+                        ourBank.PrintAllAccounts();
+                        break;
+                    case 7:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("\nPlease Select between 1 to 7\n");
+                        continue;
+
+
+
+                }
+
+
+
+
+
+
+
+            }
+
+
+
+        }
+
+
+
+        
+
+
+    }
+}
